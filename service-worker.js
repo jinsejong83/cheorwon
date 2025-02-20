530bc7743095db716f4a6d95ea6445d55acbe420
jinsejong83 <navy4117@naver.com>
2025-02-20 21:32:30 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,20 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
+
+ self.addEventListener("message", (event) => {
+  event.waitUntil(
+    new Promise((resolve, reject) => {
+       fetch("https://api.example.com/data")
+        .then((response) => response.json())
+        .then((data) => {
+          event.source.postMessage(data);  
+          resolve();
+        })
+        .catch((error) => {
+          console.error("오류 발생:", error);
+          reject(error);
+        });
+    })
+  );
+});
